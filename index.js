51cc993e4ce5192f51ba828e385d6811be074a05
michaellyu <michaellyu@outlook.com>
2018-10-01 20:40:34 +0800

    refactor: readable codes

--- index.js
+++ index.js
@@ -9,39 +9,56 @@ const rmb = (value) => {
       .split('.')
       .map(part => part.split('').map(i => i * 1));
 
+    const leftValueLength = leftValues.length; // 整数部分位数
+    const unit1 = leftValueLength - 1; // 元位
+    const unit5 = leftValueLength - 5; // 万位
+    const unit9 = leftValueLength - 9; // 亿位
+    const unit13 = leftValueLength - 13; // 万亿位
+    const unit17 = leftValueLength - 17; // 万万亿位
+    const hasLeftValue = leftValueLength > 1 || leftValues[0] > 0; // 整数部分不为0
+    const hasRightValue = rightValues && (rightValues[0] > 0 || rightValues[1] > 0); // 小数部分不为0
+    const has678Value = leftValues[unit5 - 1] > 0 || leftValues[unit5 - 2] > 0 || leftValues[unit5 - 3] > 0; // 拾万、佰万或仟万位不为0
+    const overflowIndex = leftValueLength - leftUnits.length; // 溢出位索引
+
     let leftUnitIndex = 0;
-    for (let i = leftValues.length - 1; i >= 0; i--) {
-      if (((i === leftValues.length - 1 && (leftValues.length > 1 || leftValues[0] > 0)) // 当前位为元位，且整数位不为0
-          || (i === leftValues.length - 5 && leftValues.length < 9) // 若不及亿位总是显示万位
-          || i === leftValues.length - 9
-          || i === leftValues.length - 13)
-        || (leftValues[i] > 0 && leftUnitIndex < leftUnits.length)) { // 元、万、亿，万（亿）或当前位不为0
-        fragment.unshift(leftUnits[leftUnitIndex++]);
-      } else {
-        leftUnitIndex++;
+    for (let i = leftValueLength - 1; i >= 0; i--) {
+      if (
+        (leftValues[i] > 0)
+        || (i === unit1 && hasLeftValue)
+        || (i === unit5 && has678Value)
+        || i === unit9
+        || i === unit13
+        || i === unit17
+      ) { // 元、万、亿、万亿、万万亿或当前位不为0
+        fragment.unshift(leftUnits[leftUnitIndex]);
       }
-      if (leftValues[i] > 0 || (i < leftValues.length - 1 && leftValues[i + 1] > 0)) { // 当前位或低一位不为0
+
+      if (
+        leftValues[i] > 0
+        || (leftValues[i + 1] > 0 && i !== unit5 && i !== unit9 && i !== unit13 && i !== unit17)
+        || i <= overflowIndex
+      ) { // 当前位不为0，或低一位不为0且当前位非万、亿、万亿、万万亿，或当前为溢出位
         fragment.unshift(numbers[leftValues[i]]);
       }
+
+      leftUnitIndex++;
     }
 
-    if (!rightValues || (rightValues[0] === 0 && rightValues[1] === 0)) { // 没有小数位或小数位都为0
-      fragment.push('整');
-    } else {
+    if (hasRightValue) {
       // 角
-      if (rightValues.length > 0) {
-        if (rightValues[0] > 0 || leftValues.length > 1 || leftValues[0] > 0) { // 角位不为0，或整数位不为0
-          fragment.push(numbers[rightValues[0]]);
-        }
-        if (rightValues[0] > 0) { // 角位不为0
-          fragment.push(rightUnits[0]);
-        }
+      if (rightValues[0] > 0 || hasLeftValue) { // 角位不为0，或整数位不为0
+        fragment.push(numbers[rightValues[0]]);
+      }
+      if (rightValues[0] > 0) { // 角位不为0
+        fragment.push(rightUnits[0]);
       }
       // 分
-      if (rightValues.length > 1 && rightValues[1] > 0) {
+      if (rightValues[1] > 0) {
         fragment.push(numbers[rightValues[1]]);
         fragment.push(rightUnits[1]);
       }
+    } else { // 没有小数位
+      fragment.push('整');
     }
     return fragment.join('');
   }
